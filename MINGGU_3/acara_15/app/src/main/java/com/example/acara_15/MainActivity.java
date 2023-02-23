b62c8e08cc5aada3cc9e33f28f7432e909329daa
Indra_Bagus_GolB_mobile <indrabusiness00@gmail.com>
2023-02-23 19:44:48 +0700

    minngu 3

---
+++ MINGGU_3/acara_15/app/src/main/java/com/example/acara_15/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.acara_15;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private List<mahasiswa> mahasiswaList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mahasiswaList = addData();
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        com.example.acara_15.adapter adapter = new adapter((ArrayList<mahasiswa>) mahasiswaList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+
+
+    private List<mahasiswa> addData() {
+        List<mahasiswa> mahasiswaList = new ArrayList<>();
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+        mahasiswaList.add(new mahasiswa("Indra Bagus Syah Putra", "E41210985", "081515145749"));;
+
+
+
+
+        return mahasiswaList;
+    }
+}
